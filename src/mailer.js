bafd63dbc3f4e2998caa9d795fe5b9f28cfd2162
Carlos Padial <carlos@asturnazari.com>
2022-03-14 13:09:59 +0100

    debug mail setup

--- src/mailer.js
+++ src/mailer.js
@@ -1,6 +1,48 @@
 const nodemailer = require("nodemailer");
 
 module.exports = {
+    sendDebugMail: function (subject, content, fileName, fileUrl, callback) {
+        let host = process.env.MAILHOST;
+        let user = process.env.MAILUSER;
+        let pass = process.env.MAILPASS;
+
+        let to = process.env.DEBUGMAILTO;
+
+        async function sendDebugMail() {
+            let transporter = nodemailer.createTransport({
+                host: host,
+                port: 465,
+                secure: true,
+                auth: {
+                    user: user,
+                    pass: pass,
+                },
+            });
+            let msg = {
+                from: user,
+                to: to,
+                subject: subject,
+                text: content,
+            };
+
+            if (fileName != null) {
+                msg.attachments = {
+                    filename: fileName,
+                    path: fileUrl
+                }
+            }
+
+            let message = await transporter.sendMail(msg);
+        }
+
+        sendDebugMail().then(function() {
+            console.silentLog("----- Debug message sent to: " + to);
+            if (callback) {
+                callback();
+            }
+        }).catch(console.error);
+    },
+    
     sendMail: function (subject, content, fileName, fileUrl, callback) {
         let host = process.env.MAILHOST;
         let user = process.env.MAILUSER;
